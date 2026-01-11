818117f6e64b4df1843b1376dc44527cef95bba0
adarsh-priydarshi-5646 <adarshpriydarshi5646@gmail.com>
2026-01-11 19:18:21 +0530

    fix: Restore missing imports in order.controllers.js & add .env.example files
    - Restored missing imports: Order, Shop, User, stripe, sendDeliveryOtpMailSendGrid
    - Added backend/.env.example with all required environment variables
    - Added frontend/.env.example with all required environment variables

--- backend/controllers/order.controllers.js
+++ backend/controllers/order.controllers.js
@@ -6,6 +6,11 @@
  * Supports multi-shop orders, delivery tracking & order ratings
  */
 import DeliveryAssignment from "../models/deliveryAssignment.model.js";
+import Order from "../models/order.model.js";
+import Shop from "../models/shop.model.js";
+import User from "../models/user.model.js";
+import { sendDeliveryOtpMailSendGrid } from "../utils/sendgridMail.js";
+import stripe from "../config/stripe.js";
 
 /**
  * Assigns nearby available delivery boys to order via geospatial query
